5edd3f809274a11b51049d1a355feb48b807b17f
Bulrock <maksim.volozhinskij@gmail.com>
2023-06-25 23:49:31 +0200

    feat: implement decompress command

--- src/commands/decompress.js
+++ src/commands/decompress.js
@@ -1,3 +1,24 @@
-export default async function decompressHandler() {
-  console.log('DECOMPRESS')
+import { createBrotliDecompress } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { parse, resolve } from 'path';
+import { EXEC_ERROR_MESSAGE, INVALID_INPUT_MESSGE } from '../constants/constants.js';
+
+export default async function compressHandler([pathToFile, pathToDecompressedFile]) {
+  try{
+    if(!pathToFile || !pathToDecompressedFile) {
+      console.error(INVALID_INPUT_MESSGE);
+      return
+    }
+
+    const { name } = parse(pathToFile);
+
+    const readableStream = createReadStream(resolve(pathToFile));
+    const writableStream = createWriteStream(resolve(pathToDecompressedFile, name));
+    const brotliDecompress = createBrotliDecompress();
+
+    readableStream.pipe(brotliDecompress).pipe(writableStream);
+
+  } catch (err){
+    console.error(EXEC_ERROR_MESSAGE)
+  }
 }
